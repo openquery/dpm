763fa8633e7e92d3c1686bdbe1e7e0ee379abe1f
Dormando <dormando@rydia.net>
2007-08-22 12:12:12 -0700

    remove separate defines file, toss it into startup.lua for now.

--- startup.lua
+++ startup.lua
@@ -19,7 +19,12 @@
 -- See bottom of file for connection, listening information.
 
 -- Name -> value defines for lua
-dofile ("defines.lua")
+MY_SERVER = 0
+MY_CLIENT = 1
+
+MYP_OK = 0 -- "OK" means it was handled, and okay to send packet onward.
+MYP_NOPROXY = 1 -- assume packet was handled earlier, don't copy.
+MYP_FLUSH_DISCONNECT = 2 -- Flush packet on wire and disconnect clients
 
 callback = {}
 clients  = {}
